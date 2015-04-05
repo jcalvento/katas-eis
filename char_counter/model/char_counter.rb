9974d01efd183f3ec6cde0edc72f6e096cda8e94
Julian Calvento <julian.calvento@10pines.com>
2015-04-05 13:23:48 -0300

    First commit

---
+++ char_counter/model/char_counter.rb
@@ -0,0 +1,17 @@
+class CharCounter
+
+  def self.count a_word
+    hash = {}
+    return hash if a_word.nil?
+    self.count_char_occurrences_into a_word, hash
+  end
+
+  protected
+
+  def self.count_char_occurrences_into a_word, hash
+    a_word.gsub(/\s+/, "").chars.group_by(&:chr).map {
+        |k, v| hash[k] = v.size
+    }
+    hash
+  end
+end
